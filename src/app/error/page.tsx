d0a24fbe8e8cde61ff670bc7b8eb0d28afe34b43
jochongs <joch2712@naver.com>
2025-02-20 22:12:13 +0900

    feat: Add name

---
+++ src/app/error/page.tsx
@@ -0,0 +1,13 @@
+export default function Error() {
+    return (
+        <div className="w-full max-w-[600px] mx-auto">
+            <h1
+                className="my-10 mx-10"
+                style={{
+                    fontSize: '30px',
+                }}>
+                등록되지 않은 이름입니다.
+            </h1>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import { getNameFromHost } from '@/util/getNameFromHost';
+import { prisma } from '@/util/prisma';
 import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
     const headerList = headers();
     const host = (await headerList).get('host');
 
-    console.log(getNameFromHost(host));
+    const userName = getNameFromHost(host);
 
-    return <div></div>;
+    if (!userName) {
+        redirect('/error');
+    }
+
+    const user = await prisma.user.findFirst({ where: { userName } });
+
+    if (!user) {
+        redirect('/error');
+    }
+
+    return (
+        <div className="w-full max-w-[600px] mx-auto">
+            <img src={`https://congraduation.s3.ap-northeast-2.amazonaws.com${user.imgPath}`} alt="" />
+        </div>
+    );
 }
